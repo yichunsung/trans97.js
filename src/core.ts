10ca27333c84e61dd9202b0d028abee9ad5890b8
YiChun Sung <ycsung.r@gmail.com>
2021-09-19 02:32:27 +0800

    Updated core.ts

--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,5 @@
 import { TWD97 } from './twd97';
+import { WGS84 } from './wgs84';
 
 export class Trans97 {
 
@@ -8,6 +9,8 @@ export class Trans97 {
     const { type } = options;
     if (type === 'twd97') {
       this.service = new TWD97();
+    } else if (type === 'wgs84') {
+      this.service = new WGS84();
     }
   }
 
